4b416e169a97509c543efaa49131812c0f72e0a4
xgingerbiscuitX <78746317+xgingerbiscuitX@users.noreply.github.com>
2023-05-17 17:11:50 +0100

    Add files via upload

---
+++ fakeDalUnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("fakeDalUnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fakeDalUnitTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68afbdc3-1c6e-470f-91b1-50e757686d1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ fakeDalUnitTesting/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using BusinessEntities;
+using DataAccessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+
+namespace fakeDalUnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void LoginTestDelay()
+        {
+            ArrayList userList = new ArrayList();
+            IDataLayer _Datalayer; 
+            _Datalayer = IDatalayer.GetInstance();
+            userList = _Datalayer.getAllUsers();
+
+            string username = "Lukasz4282";
+            string password = "awesome123";
+
+            Staff CurrentUser = null;
+
+            foreach (Staff staff in userList)
+            {
+                //MessageBox.Show("What is being checked: " + staff.UserName + " - " + staff.Password + "\nWhat You typed in: " + username + " - " + password);
+                if (username == staff.UserName && Crypt.Encrypt(password) == staff.Password)
+                {
+                    CurrentUser = staff;
+                }
+            }
+
+            Assert.IsNotNull(CurrentUser);
+        }
+    }
+}
